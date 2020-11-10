d1354c20fb490f7a478107afde58d4f0c461cf7f
karthik2304 <40534953+karthik2304@users.noreply.github.com>
2020-11-10 13:23:52 +0530

    Go Program beginner to advanced
    already uploaded as sub directeories in devops_daily repo

---
+++ Basic go and advanced/BODMAS.go
@@ -0,0 +1,11 @@
+package main
+ 
+import "fmt"
+ 
+func main() {
+    fmt.Println("1 + 2 = ",1+2)      // Addition
+    fmt.Println("5 * 2 = ",5*2)       // Multiplication
+    fmt.Println("10 - 2 = ",10-2)   // Subtraction
+    fmt.Println("10 / 2 = ",10/2)    // Division
+    fmt.Println("10 % 2 = ",10%2)   // Remainder
+}
---
+++ Basic go and advanced/Hashtable.go
@@ -0,0 +1,19 @@
+package main
+ 
+import (
+"fmt"
+)
+ 
+func main() {
+    var country map[int]string
+    country = make(map[int] string)
+    country[1]="India"
+    country[2]="China"
+    country[3]="Pakistan"
+    country[4]="Germany"
+    country[5]="Australia"
+    country[6]="Indonesia"
+    for i, j := range country {
+        fmt.Printf("Key: %d Value: %s\n", i, j)
+    }
+}
